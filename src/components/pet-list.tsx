52e47c7c9353613c2650c8b71ad55d431cd956ba
Choxy5 <kristijan.blazevic000@gmail.com>
2024-07-23 22:37:36 +0200

    Fetch data and Context API implementation

---
+++ src/components/pet-list.tsx
@@ -0,0 +1,28 @@
+'use client';
+import { usePetContext } from '@/lib/hooks';
+import Image from 'next/image';
+
+export default function PetList() {
+  const { pets } = usePetContext();
+
+  return (
+    <ul className="bg-white border-b border-black/[0.08]">
+      {pets.map((pet) => (
+        <>
+          <li key={pet.id}>
+            <button className="flex items-center h-[70px] w-full cursor-pointer px-5 text-base gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+              <Image
+                src={pet.imageUrl}
+                alt="Pet image"
+                width={45}
+                height={45}
+                className="w-[45px] h-[45px] rounded-full object-cover"
+              />
+              <p className="font-semibold">{pet.name}</p>
+            </button>
+          </li>
+        </>
+      ))}
+    </ul>
+  );
+}
