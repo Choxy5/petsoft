6ceade752e5d1d6b477bebd838ad5c2cbc315c6c
Choxy5 <kristijan.blazevic000@gmail.com>
2024-07-23 15:25:14 +0200

    Shadcn-ui setup

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 import Logo from '@/components/logo';
+import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -21,8 +23,13 @@ export default function Home() {
           Use PetSoft to easily keep track of pets udner your care. Get lifetime
           access for $299.
         </p>
-        <div className="mt-10">
-          
+        <div className="mt-10 space-x-3">
+          <Button asChild>
+            <Link href="/signup">Get Started</Link>
+          </Button>
+          <Button variant="secondary" asChild>
+            <Link href="/login">Log in</Link>
+          </Button>
         </div>
       </div>
     </main>
