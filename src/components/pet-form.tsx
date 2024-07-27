021fb6b13ce2a17f5ce4c4602aed921ff28bdd3e
Choxy5 <kristijan.blazevic000@gmail.com>
2024-07-27 12:47:52 +0200

    AddPet Server Action and replace onSubmit with action in form

--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { addPet } from '@/actions/actions';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
@@ -14,32 +15,10 @@ export default function PetForm({
   actionType,
   onFormSubmission,
 }: PetFormProps) {
-  const { handleAddPet, selectedPet, handleEditPet } = usePetContext();
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-
-    const formData = new FormData(event.currentTarget);
-    const pet = {
-      name: formData.get('name') as string,
-      ownerName: formData.get('ownerName') as string,
-      imageUrl:
-        (formData.get('imageUrl') as string) ||
-        'https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png',
-      age: +(formData.get('age') as string),
-      notes: formData.get('notes') as string,
-    };
-    if (actionType === 'add') {
-      handleAddPet(pet);
-    } else if (actionType === 'edit') {
-      handleEditPet(selectedPet!.id, pet);
-    }
-
-    onFormSubmission();
-  };
+  const { selectedPet } = usePetContext();
 
   return (
-    <form className="flex flex-col" onSubmit={handleSubmit}>
+    <form className="flex flex-col" action={addPet}>
       <div className="space-y-3">
         <div className="space-y-1">
           <Label htmlFor="name">Name</Label>
