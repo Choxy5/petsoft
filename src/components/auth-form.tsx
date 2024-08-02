bec2445ae173c151b289c496cbfa983d27bb1b69
Choxy5 <kristijan.blazevic000@gmail.com>
2024-08-02 14:35:54 +0200

    Updating Prisma Schema with User model

---
+++ src/components/auth-form.tsx
@@ -0,0 +1,24 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+type AuthFormProps = {
+  type: 'logIn' | 'signUp';
+};
+
+export default function AuthForm({ type }: AuthFormProps) {
+  return (
+    <form>
+      <div className="space-y-1">
+        <Label htmlFor="email">Email</Label>
+        <Input id="email" type="email" />
+      </div>
+      <div className="space-y-1 mb-4 mt-2">
+        <Label htmlFor="password">Password</Label>
+        <Input id="password" type="password" />
+      </div>
+
+      <Button>{type === 'logIn' ? 'Log In' : 'Sign Up'}</Button>
+    </form>
+  );
+}
