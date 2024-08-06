586b1ab66d2eda7f7a1ad3240f9b0259573d201b
Choxy5 <kristijan.blazevic000@gmail.com>
2024-08-06 21:00:27 +0200

    SearchParams for success and canceled payment

--- src/app/(auth)/payment/page.tsx
+++ src/app/(auth)/payment/page.tsx
@@ -3,18 +3,31 @@ import { createCheckoutSession } from '@/actions/actions';
 import H1 from '@/components/h1';
 import { Button } from '@/components/ui/button';
 
-export default function Page() {
+export default function Page({ searchParams }) {
   return (
     <main className="flex flex-col items-center space-y-10">
       <H1>PetSoft access requires payment</H1>
 
-      <Button
-        onClick={async () => {
-          await createCheckoutSession();
-        }}
-      >
-        Buy lifetime access for 299€
-      </Button>
+      {!searchParams.success && (
+        <Button
+          onClick={async () => {
+            await createCheckoutSession();
+          }}
+        >
+          Buy lifetime access for 299€
+        </Button>
+      )}
+
+      {searchParams.success && (
+        <p className="text-sm text-green-600">
+          Payment successful! You now have lifetime access to PetSoft.
+        </p>
+      )}
+      {searchParams.canceled && (
+        <p className="text-sm text-red-600">
+          Payment canceled. You can try again.
+        </p>
+      )}
     </main>
   );
 }
